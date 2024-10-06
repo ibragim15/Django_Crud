c63d962ab2bae469fc878a38049a0bbe726237d9
ibra <said.daurbekov@icloud.com>
2024-10-06 10:47:53 +0300

    upgrade settings

--- mySite/settings.py
+++ mySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@%79$%#s!@ynz)1do76m&mdc2+or%+6dg*hwch79*8%s@^9vn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
