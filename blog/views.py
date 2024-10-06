f715fb4792219fce3e58bf0a50c01defc5cfb7a1
ibra <said.daurbekov@icloud.com>
2024-10-06 10:24:28 +0300

    create django project

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import MyPost
+from django.utils import timezone
+
+def index (request):
+    posts = MyPost.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+# Create your views here.
